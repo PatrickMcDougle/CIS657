44ce40e19a014e8f8402fde2d9ba2e29ff0c14d5
Patrick McDougle <Patrick.McDougle@gmail.com>
2020-04-16 21:23:07 -0600

    My version of lab 2 with 3 different solutions.

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -3,22 +3,133 @@
 #include <xinu.h>
 #include <stdio.h>
 
+void number_print(sid32, sid32);
+void sentance_print(sid32, sid32);
+void number_print2(sid32);
+void sentance_print2(sid32);
+void number_print3(sid32, sid32);
+void sentance_print3(sid32, sid32);
+
+int counter = 0;
+
+#define _COUNTER_MAX 2000
+
 /* main - just saying hello, then exiting */
 int main(int argc, char const *argv[])
 {
-	printf("/* ---------- ---------- ---------- ---------- */\n");
-	printf("    _____      _        _      _    \n");
-	printf("   |  __ \\    | |      (_)    | |   \n");
-	printf("   | |__) |_ _| |_ _ __ _  ___| | __\n");
-	printf("   |  ___/ _` | __| '__| |/ __| |/ /\n");
-	printf("   | |  | (_| | |_| |  | | (__|   < \n");
-	printf("   |_|   \\__,_|\\__|_|  |_|\\___|_|\\_\\\n");
-	printf("/* ---------- ---------- ---------- ---------- */\n\n");
+	sid32 print_number;
+	sid32 print_sentance;
+	sid32 print_sem;
 
-	resume(create(sndA, 1024, 20, "sending A", 0));
-	resume(create(sndB, 1024, 40, "sending B", 0));
+	printf("/* ----------- ---------- ----------- ---------- ----------- */\n");
+	printf("    ___________  _______     ___       ___  ___      _  _    \n");
+	printf("   |           ||   ____|   /   \\     |   \\/   |    | || |   \n");
+	printf("    ---|  |---- |  |__     /  ^  \\    |  \\  /  |    | || |_  \n");
+	printf("       |  |     |   __|   /  /_\\  \\   |  |\\/|  |    |__   _| \n");
+	printf("       |  |     |  |____ /  _____  \\  |  |  |  |       | |   \n");
+	printf("       |__|     |_______/__/     \\__\\ |__|  |__|       |_|   \n");
+	printf("/* ----------- ---------- ----------- ---------- ----------- */\n");
+
+	// print_number = semcreate(0);
+	// print_sentance = semcreate(1);
+
+	// resume(create(number_print, 1024, 20, "print numbers", 2, print_number, print_sentance));
+	// resume(create(sentance_print, 1024, 20, "print sentance", 2, print_number, print_sentance));
+
+	// print_sem = semcreate(20);
+
+	// resume(create(number_print2, 1024, 20, "print numbers", 1, print_sem));
+	// resume(create(sentance_print2, 1024, 20, "print sentance", 1, print_sem));
+
+	print_number = semcreate(0);
+	print_sentance = semcreate(21);
+
+	resume(create(number_print3, 1024, 20, "print numbers", 2, print_number, print_sentance));
+	resume(create(sentance_print3, 1024, 20, "print sentance", 2, print_number, print_sentance));
 
 	return OK;
+}
+
+void number_print(
+	sid32 print_number,
+	sid32 print_sentance)
+{
+	// int counter = 0;
+	int five = 0;
+
+	wait(print_sentance);
+	for (counter = 1; counter < 22; ++counter)
+	{
+		printf("%d ", counter);
+	}
+	signal(print_number);
+
+	do
+	{
+		five = 0;
+		wait(print_sentance);
+		while (five++ < 5)
+		{
+			printf("%d ", counter);
+		}
+		signal(print_number);
+	} while (++counter < _COUNTER_MAX);
+}
+
+void sentance_print(
+	sid32 print_number,
+	sid32 print_sentance)
+{
+	do
+	{
+		wait(print_number);
+		printf("Team 4 Rocks! ");
+		signal(print_sentance);
+	} while (counter < _COUNTER_MAX);
+}
+
+void number_print2(
+	sid32 print_sem)
+{
+	do
+	{
+		printf("%d ", ++counter);
+		wait(print_sem);
+	} while (counter < _COUNTER_MAX);
+}
+
+void sentance_print2(
+	sid32 print_sem)
+{
+	do
+	{
+		printf("Team 4 Rocks! ");
+		signaln(print_sem, 5);
+	} while (counter < _COUNTER_MAX);
+}
+
+void number_print3(
+	sid32 print_number,
+	sid32 print_sentance)
+{
+	do
+	{
+		wait(print_sentance);
+		printf("%d ", ++counter);
+		signal(print_number);
+	} while (counter < _COUNTER_MAX);
+}
+
+void sentance_print3(
+	sid32 print_number,
+	sid32 print_sentance)
+{
+	do
+	{
+		wait(print_number);
+		printf("Team 4 Rocks! ");
+		signaln(print_sentance, 5);
+	} while (counter < _COUNTER_MAX);
 }
 
 // int main(int argc, char **argv)
