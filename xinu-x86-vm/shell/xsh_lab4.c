f00eb7bcbaeda478d232fb467eb29726cdbcaf8f
Patrick McDougle <Patrick.McDougle@gmail.com>
2020-05-04 21:44:06 -0600

    formatting of the makefile. Added some randomness to the output. Set the random seed in main file.

--- xinu-x86-vm/shell/xsh_lab4.c
+++ xinu-x86-vm/shell/xsh_lab4.c
@@ -12,15 +12,41 @@ shellcmd xsh_lab4(int nargs, char *args[])
 {
 	sid32 partnerAsSID;
 	sid32 partnerBsSID;
-
 	partnerAsSID = semcreate(0);
 	partnerBsSID = semcreate(0);
 
-	pid32 aPID = create(a, 1024, 100, "lab4 A", 2, partnerAsSID, partnerBsSID);
-	pid32 bPID = create(b, 1024, 101, "lab4 B", 2, partnerBsSID, partnerAsSID);
+	int32 pri1;
+	int32 pri2;
+	pri1 = 100 + (rand() % 10);
+	do
+	{
+		pri2 = 100 + (rand() % 10);
+	} while (pri1 == pri2); // while loop makes sure the priorites are not the same.
 
-	resume(bPID);
-	resume(aPID);
+	char lab1[20];
+	char lab2[20];
+
+	sprintf(lab1, "lab4 A - %d", pri1);
+	sprintf(lab2, "lab4 B - %d", pri2);
+
+	// kprintf(lab1);
+	// printf("\n");
+	// kprintf(lab2);
+	// printf("\n");
+
+	pid32 aPID = create(a, 1024, pri1, lab1, 2, partnerAsSID, partnerBsSID);
+	pid32 bPID = create(b, 1024, pri2, lab2, 2, partnerBsSID, partnerAsSID);
+
+	if (rand() % 2 == 0)
+	{
+		resume(bPID);
+		resume(aPID);
+	}
+	else
+	{
+		resume(aPID);
+		resume(bPID);
+	}
 
 	printf("\n");
 
--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -1,11 +1,15 @@
 /*  main.c  - main */
 
 #include <xinu.h>
+#include <string.h>
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
 	uint32 retval;
+
+	// set the random seed so we get same results over and over again
+	srand(117);
 
 	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
 
@@ -18,7 +22,7 @@ int main(int argc, char **argv)
 		kprintf("\n\n\rMain process recreating shell\n\n\r");
 		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
 	}
-	
+
 	while (1)
 	{
 		// do nothing, run forever.
