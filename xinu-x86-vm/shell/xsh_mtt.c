57c3e6cb9adc72678f19ad54f238a3f9fb8c9856
Patrick McDougle <Patrick.McDougle@gmail.com>
2020-05-18 17:37:22 -0600

    - crated process that runs forever.

--- xinu-x86-vm/shell/xsh_mtt.c
+++ xinu-x86-vm/shell/xsh_mtt.c
@@ -3,70 +3,71 @@
 #include <xinu.h>
 #include <stdio.h>
 
+// function declarations:
+void runforever(int16);
 
-
-/*------------------------------------------------------------------------
+	/*------------------------------------------------------------------------
  * xsh_mtt1 - Creates a new process with a user specified priority 
  *   (or default) that starts running in an empty infinite loop after
  *   announcing its PID.
  *------------------------------------------------------------------------
  */
-shellcmd xsh_mtt1(int nargs, char *args[])
+	shellcmd xsh_mtt1(int nargs, char *args[])
 {
-    pri16 priority = INITPRIO;
+	pri16 priority = INITPRIO;
 
-    // Output info for '--help' argument
-    if (nargs == 2)
-    {
-        if (strncmp(args[1], "--help", 7) == 0)
-        {
-            printf("Usage: %s <priority>\n\n", args[0]);
-            printf("Description:\n");
-            printf("\tStarts a new process that runs forever with the given priority.\n");
-            printf("Options:\n");
-            printf("\t<priority>\tThe priority value to use when creating the forever\n");
-            printf("\t\t\tprocess.  Valid values are in the range: 1 to 127.\n");
-            printf("\t\t\tDefault value is 20.\n");
-            printf("\t--help    \tDisplay this help and exit\n");
-            return OK;
-        }
+	// Output info for '--help' argument
+	if (nargs == 2)
+	{
+		if (strncmp(args[1], "--help", 7) == 0)
+		{
+			printf("Usage: %s <priority>\n\n", args[0]);
+			printf("Description:\n");
+			printf("\tStarts a new process that runs forever with the given priority.\n");
+			printf("Options:\n");
+			printf("\t<priority>\tThe priority value to use when creating the forever\n");
+			printf("\t\t\tprocess.  Valid values are in the range: 1 to 127.\n");
+			printf("\t\t\tDefault value is 20.\n");
+			printf("\t--help    \tDisplay this help and exit\n");
+			return OK;
+		}
 
-        if (is_positive_integer(args[1]) == TRUE)
-        {
-            // going to assume the input is an integer.
-            priority = atoi(args[1]);
+		if (is_positive_integer(args[1]) == TRUE)
+		{
+			// going to assume the input is an integer.
+			priority = atoi(args[1]);
 
-            // printf("Priority updated: [%d]\n", priority);
+			// printf("Priority updated: [%d]\n", priority);
 
-            // check to make sure the priority is valid.
-            if (priority < 1 || priority > 127)
-            {
-                fprintf(stderr, "%s: Incorrect argument - value needs to be in the range of 1 to 127 inclusive.\n", args[0]);
-                fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
-                return SYSERR;
-            }
-        }
-        else
-        {
-            fprintf(stderr, "%s: Incorrect argument - value is not a postive number\n", args[0]);
-            fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
-            return SYSERR;
-        }
-    }
+			// check to make sure the priority is valid.
+			if (priority < 1 || priority > 127)
+			{
+				fprintf(stderr, "%s: Incorrect argument - value needs to be in the range of 1 to 127 inclusive.\n", args[0]);
+				fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
+				return SYSERR;
+			}
+		}
+		else
+		{
+			fprintf(stderr, "%s: Incorrect argument - value is not a postive number\n", args[0]);
+			fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
+			return SYSERR;
+		}
+	}
 
-    // Check argument count
-    else if (nargs > 2)
-    {
-        fprintf(stderr, "%s: Incorrect number of arguments\n", args[0]);
-        fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
-        return SYSERR;
-    }
+	// Check argument count
+	else if (nargs > 2)
+	{
+		fprintf(stderr, "%s: Incorrect number of arguments\n", args[0]);
+		fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
+		return SYSERR;
+	}
 
-    pid32 pid_runforever = create(runforever, 1024, priority, "runforever", 1, priority);
+	pid32 pid_runforever = create(runforever, 1024, priority, "patrick", 1, priority);
 
-    resume(pid_runforever);
+	resume(pid_runforever);
 
-    return 0;
+	return 0;
 }
 
 /*------------------------------------------------------------------------
@@ -228,4 +229,20 @@ shellcmd xsh_mtt6(int nargs, char *args[])
 	printf("\n");
 
 	return 0;
+}
+
+/* ---------- ---------- ---------- ---------- ---------- ----------
+ * runforever - shell command to do whatever you want it to do.
+ * ---------- ---------- ---------- ---------- ---------- ----------
+ */
+void runforever(
+	int16 priority /* Priority value for this process */
+)
+{
+	printf("priority & pid: [%d,%d]\n", priority, getpid());
+
+	while (TRUE)
+	{
+		// do nothing at this time.
+	}
 }
