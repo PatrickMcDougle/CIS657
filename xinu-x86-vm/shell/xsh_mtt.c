eafaf4bb0293439dbf2ee02319ef4ba144b09406
Patrick McDougle <Patrick.McDougle@gmail.com>
2020-05-18 17:33:37 -0600

    - setup of part 1's create command.

--- xinu-x86-vm/shell/xsh_mtt.c
+++ xinu-x86-vm/shell/xsh_mtt.c
@@ -3,6 +3,8 @@
 #include <xinu.h>
 #include <stdio.h>
 
+
+
 /*------------------------------------------------------------------------
  * xsh_mtt1 - Creates a new process with a user specified priority 
  *   (or default) that starts running in an empty infinite loop after
@@ -11,20 +13,60 @@
  */
 shellcmd xsh_mtt1(int nargs, char *args[])
 {
-	int32 i; /* walks through args array	*/
+    pri16 priority = INITPRIO;
 
-	if (nargs > 1)
-	{
-		printf("%s", args[1]);
+    // Output info for '--help' argument
+    if (nargs == 2)
+    {
+        if (strncmp(args[1], "--help", 7) == 0)
+        {
+            printf("Usage: %s <priority>\n\n", args[0]);
+            printf("Description:\n");
+            printf("\tStarts a new process that runs forever with the given priority.\n");
+            printf("Options:\n");
+            printf("\t<priority>\tThe priority value to use when creating the forever\n");
+            printf("\t\t\tprocess.  Valid values are in the range: 1 to 127.\n");
+            printf("\t\t\tDefault value is 20.\n");
+            printf("\t--help    \tDisplay this help and exit\n");
+            return OK;
+        }
 
-		for (i = 2; i < nargs; i++)
-		{
-			printf(" %s", args[i]);
-		}
-	}
-	printf("\n");
+        if (is_positive_integer(args[1]) == TRUE)
+        {
+            // going to assume the input is an integer.
+            priority = atoi(args[1]);
 
-	return 0;
+            // printf("Priority updated: [%d]\n", priority);
+
+            // check to make sure the priority is valid.
+            if (priority < 1 || priority > 127)
+            {
+                fprintf(stderr, "%s: Incorrect argument - value needs to be in the range of 1 to 127 inclusive.\n", args[0]);
+                fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
+                return SYSERR;
+            }
+        }
+        else
+        {
+            fprintf(stderr, "%s: Incorrect argument - value is not a postive number\n", args[0]);
+            fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
+            return SYSERR;
+        }
+    }
+
+    // Check argument count
+    else if (nargs > 2)
+    {
+        fprintf(stderr, "%s: Incorrect number of arguments\n", args[0]);
+        fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
+        return SYSERR;
+    }
+
+    pid32 pid_runforever = create(runforever, 1024, priority, "runforever", 1, priority);
+
+    resume(pid_runforever);
+
+    return 0;
 }
 
 /*------------------------------------------------------------------------
@@ -61,7 +103,7 @@ shellcmd xsh_mtt3(int nargs, char *args[])
 {
 	// THIS CODE WAS COPIED OUT OF xsh_ps.c with minor tweeks.
 	// //// //// /// ////// /// // //////// //// ///// ///////
-	
+
 	struct procent *prptr; /* pointer to process		*/
 	int32 i;			   /* index into proctabl		*/
 	char *pstate[] = {	   /* names for process states	*/
