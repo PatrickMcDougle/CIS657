b160b5f6c7e2e8d5e7cb8bad906fd6a2acb4784b
Patrick McDougle <Patrick.McDougle@gmail.com>
2020-05-26 08:45:29 -0600

    - Start the creation of lab5

---
+++ xinu-x86-vm/system/lab5.c
@@ -0,0 +1,11 @@
+/* lab5.c - lab5_process2, lab5_process3, lab5_process4 */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  lab5_process2 - process prints ID only once and then sleeps for 15 seconds
+ *------------------------------------------------------------------------
+ */
+void lab5_process2(void) /* assumes interrupts are disabled	*/
+{
+}
