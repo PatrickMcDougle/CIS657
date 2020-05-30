777ee20a67ba63547ea8575cb674a7d74982e152
Patrick McDougle <Patrick.McDougle@gmail.com>
2020-05-30 15:46:37 -0600

    - farmatted files.

--- xinu-x86-vm/system/receive.c
+++ xinu-x86-vm/system/receive.c
@@ -6,20 +6,21 @@
  *  receive  -  wait for a message and return the message to the caller
  *------------------------------------------------------------------------
  */
-umsg32	receive(void)
+umsg32 receive(void)
 {
-	intmask	mask;			/* saved interrupt mask		*/
-	struct	procent *prptr;		/* ptr to process' table entry	*/
-	umsg32	msg;			/* message to return		*/
+	intmask mask;		   /* saved interrupt mask			*/
+	struct procent *prptr; /* ptr to process' table entry	*/
+	umsg32 msg;			   /* message to return				*/
 
 	mask = disable();
 	prptr = &proctab[currpid];
-	if (prptr->prhasmsg == FALSE) {
+	if (prptr->prhasmsg == FALSE)
+	{
 		prptr->prstate = PR_RECV;
-		resched();		/* block until message arrives	*/
+		resched(); /* block until message arrives			*/
 	}
-	msg = prptr->prmsg;		/* retrieve message		*/
-	prptr->prhasmsg = FALSE;	/* reset message flag		*/
+	msg = prptr->prmsg;		 /* retrieve message			*/
+	prptr->prhasmsg = FALSE; /* reset message flag			*/
 	restore(mask);
 	return msg;
 }
